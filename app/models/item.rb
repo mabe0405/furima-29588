3dc6c460c4a8d64a8174f047250a243e80713aba
mabe0405 <m.abe.prog@gmail.com>
2020-11-27 19:27:51 +0900

    Itemモデルのバリデーション、アソシエーション設置

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,23 @@ class Item < ApplicationRecord
 has_one_attached :image
 belongs_to :user
 
+extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+	belongs_to :status
+	belongs_to :shipping_cost
+	belongs_to :shipping_source
+	belongs_to :delivery_date
+
+with_options presence: true do
+  validates :image
+  validates :name
+  validates :description
+  validates :category_id, numericality: { other_than: 1 }
+  validates :shipping_cost_id, numericality: { other_than: 1 }
+  validates :shipping_source_id, numericality: { other_than: 1 }
+  validates :delivery_date_id, numericality: { other_than: 1 }
+  validates :price,format: { with: 3[0-9][0-9]|9[0-9][0-9][0-9][0-9][0-9][0-9] ,message: 'Out of setting range' }
+end
+
+
 end
