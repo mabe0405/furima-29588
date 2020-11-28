a71dd5d9fdb6ff68bf086c83c6045113540b8c90
mabe0405 <m.abe.prog@gmail.com>
2020-11-28 13:02:47 +0900

    バリデーション訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,11 +13,13 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
-    validates :category_id, numericality: { other_than: 1, message: 'Select' }
-    validates :status_id, numericality: { other_than: 1, message: 'Select' }
-    validates :shipping_cost_id, numericality: { other_than: 1, message: 'Select' }
-    validates :shipping_source_id, numericality: { other_than: 0, message: 'Select' }
-    validates :delivery_date_id, numericality: { other_than: 1, message: 'Select' }
+    with_options numericality: { other_than: 1, message: 'Select' } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_cost_id
+      validates :delivery_date_id
+    end
+    validates :shipping_source_id,numericality: { other_than: 0, message: 'Select' }
     validates :price, numericality: { message: 'Half-width number' }
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
   end
