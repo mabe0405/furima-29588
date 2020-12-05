0752e6c41e8af278bf4df270e924afa9497e00b5
mabe0405 <m.abe.prog@gmail.com>
2020-12-05 15:47:18 +0900

    コントローラー作成

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,26 @@
+class PurchasesController < ApplicationController
+  # before_action :authenticate_user!, except: [:index, :show]
+  # before_action :set_item, only: [:show , :edit, :update, :destroy]
+
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
